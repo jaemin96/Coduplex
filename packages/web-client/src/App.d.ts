baf280c9a523dc48facdb510320a2466c6515550
jaemin96 <77824886+jaemin96@users.noreply.github.com>
2025-08-30 16:07:50 +0900

    fjeikqroqe

---
+++ packages/web-client/src/App.d.ts
@@ -0,0 +1,3 @@
+import "@my/ui/style.css";
+declare function App(): import("react/jsx-runtime").JSX.Element;
+export default App;
--- packages/web-client/src/global.d.ts
+++ packages/web-client/src/global.d.ts
@@ -1,1 +1,2 @@
 declare module "@my/ui";
+declare module "@my/ui/*.css";
