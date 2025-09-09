f70bbd2014ba52ab9dd7b2c2dfd9013bb684d441
jaemin96 <lemon__96@naver.com>
2025-09-09 17:52:29 +0900

    [dawntech] feat(migrate): nextjs project init
    - 프로젝트 마이그레이션 준비를 위해 Nextjs 기본 프로젝트 구성
    - 기본적인 scss, public 정적 자산들 추가
    - 기본 생성 파일 구조 변경 및 기본 골격 구축
    
    Refs #3

---
+++ packages/dawntech/src/components/footer/footer.tsx
@@ -0,0 +1,15 @@
+type DTfooterProps = {};
+
+const DTfooter: React.FC<DTfooterProps> = (props) => {
+  /* === state === */
+
+  /* === handler === */
+
+  return (
+    <>
+      <div id={"footer"}>footer</div>
+    </>
+  );
+};
+
+export default DTfooter;
