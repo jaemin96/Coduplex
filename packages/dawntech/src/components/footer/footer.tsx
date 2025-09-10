c6062d625667a7c681405fbb9f18c01ba2fdbac4
jaemin96 <lemon__96@naver.com>
2025-09-10 11:08:33 +0900

    [dawntech] feat(components): add footer DOM & style
    - footer 기본 DOM 구성, 스타일 작업
    - scss 도운테크 패키지 구조에 맞게 호출 로직 개선
    - scss 메인 import 누락 부분 반영
    
    Refs #3

--- packages/dawntech/src/components/footer/footer.tsx
+++ packages/dawntech/src/components/footer/footer.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { siteConfig } from "@/config";
+
 type DTfooterProps = {};
 
 const DTfooter: React.FC<DTfooterProps> = (props) => {
@@ -7,7 +10,68 @@ const DTfooter: React.FC<DTfooterProps> = (props) => {
 
   return (
     <>
-      <div id={"footer"}>footer</div>
+      <div className="footer">
+        <div className="footer-container">
+          {/* 회사명 */}
+          <div className="company">
+            <h1>{siteConfig.company ?? "-"}</h1>
+          </div>
+          {/* 대표 & 사업자등록번호 */}
+          <div className="rep">
+            <div className="ceo">
+              <span className="rep-title">{`대표`}</span>
+              <span className="rep-content">{siteConfig.ceo ?? "-"}</span>
+            </div>
+            <div className="companyRN">
+              <span className="rep-title">{`사업자등록번호`}</span>
+              <span className="rep-content">
+                {siteConfig.businessNumber ?? "-"}
+              </span>
+            </div>
+          </div>
+          {/* 주소 | 전화 & 팩스 | 이메일 */}
+          <div className="contact">
+            <div className="contact-address">
+              <span className="contact-title">{`주소`}</span>
+              <span className="contact-content">
+                {siteConfig.address ?? "-"}
+              </span>
+            </div>
+            <div className="contact-call call">
+              <div className="call-tel">
+                <span className="contact-title">{`전화`}</span>
+                <span className="contact-content">{siteConfig.tel ?? "-"}</span>
+              </div>
+              <div className="call-fax">
+                <span className="contact-title">{`팩스`}</span>
+                <span className="contact-content">{siteConfig.fax ?? "-"}</span>
+              </div>
+            </div>
+            <div className="contact-email">
+              <span className="contact-title">{`이메일`}</span>
+              <span className="contact-content">{siteConfig.email ?? "-"}</span>
+            </div>
+          </div>
+          {/* 개인정보취급방침 | 이메일무단수집거부 | 사이트맵 */}
+          <div className="service">
+            <ul className="service-items">
+              <li className="privacy">
+                <Link href="/service/privacy">{`개인정보취급방침`}</Link>
+              </li>
+              <li className="reject">
+                <Link href="/service/reject">{`이메일무단수집거부`}</Link>
+              </li>
+              <li className="sitemap">
+                <Link href="/service/sitemap">{`사이트맵`}</Link>
+              </li>
+            </ul>
+          </div>
+          {/* 구분선 */}
+          <div className="boundary"></div>
+          {/* copyright */}
+          <span className="copyright">{siteConfig.copyright ?? "-"}</span>
+        </div>
+      </div>
     </>
   );
 };
---
+++ packages/dawntech/src/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  company: "도운테크",
+  ceo: "김종운",
+  businessNumber: "124-46-70585",
+  address: "445-300) 경기도 화성시 기산동 217-8번지",
+  tel: "070-8871-2866",
+  fax: "0505-810-1871",
+  email: "dawntech@dawnth.co.kr",
+  copyright: "Copyright 2025. Dawntech. All rights reserved.",
+} as const;
