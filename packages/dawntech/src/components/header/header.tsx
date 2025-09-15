68c55582554324be14953e81af034ea78174f420
jaemin96 <lemon__96@naver.com>
2025-09-15 10:07:46 +0900

    [dawntech] feat(header): add header component
    - 기본 헤더 레이아웃 구조 세팅
    - 간단한 스타일 코드 추가
    - 스타일 모듈에 header 스타일 연동
    
    Refs #3

--- packages/dawntech/src/components/header/header.tsx
+++ packages/dawntech/src/components/header/header.tsx
@@ -1,13 +1,34 @@
-type DTheaderProps = {};
+"use client";
 
-const DTheader: React.FC<DTheaderProps> = (props) => {
-  /* === state === */
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
-  /* === handler === */
+const DTheader = () => {
+  const [visible, setVisible] = useState<boolean>(false);
+
+  const toggleMenu = () => setVisible((prev) => !prev);
+
+  useEffect(() => {
+    return () => {
+      // clear
+    };
+  }, []);
 
   return (
     <>
-      <div id={"header"}>header</div>
+      <div className="container">
+        {/* 로고 영역 */}
+        <div className="logo">
+          <Image src="/imgs/en.png" width={130} height={50} alt="home-logo" />
+        </div>
+
+        {/* 내비게이션 영역 */}
+        <nav className="nav">
+          <a href="/about" className="nav-item">
+            About
+          </a>
+        </nav>
+      </div>
     </>
   );
 };
