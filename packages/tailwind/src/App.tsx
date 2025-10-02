40d35e08de886fcc9cccae09e054e16502957333
jaemin96 <lemon__96@naver.com>
2025-10-02 17:27:24 +0900

    [tailwind] feat(shadcn): add UI component - sidebar
    - add example code - sidebar
    
    Refs #16

--- packages/tailwind/src/App.tsx
+++ packages/tailwind/src/App.tsx
@@ -1,12 +1,23 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import Layout from "./app/layout";
 
 function App() {
   return (
     <>
-      <div className="text-3xl font-bold underline">hello Tailwind + Vite</div>
-      <Button onClick={() => {
-        console.log("haha");
-      }}>Click me</Button>
+      <Layout>
+        <main className="pl-4">
+          <div className="text-3xl font-bold underline">
+            hello Tailwind + Vite
+          </div>
+          <Button
+            onClick={() => {
+              console.log("haha");
+            }}
+          >
+            Click me
+          </Button>
+        </main>
+      </Layout>
     </>
   );
 }
