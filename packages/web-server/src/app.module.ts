00e68cfb92b5f4bf88afddc40849e040cda2cb6a
jaemin96 <lemon__96@naver.com>
2025-11-10 17:04:05 +0900

    [web-server] feat(board): add module - board
    - 기본 스웨거 사용 환경 구축
    - 임시 메모리 list, dto 구축
    - board(게시판) 관련 기본 CRUD 서비스, 핸들러 구축
    
    Refs #18

--- packages/web-server/src/app.module.ts
+++ packages/web-server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, BoardModule],
   providers: [],
 })
 export class AppModule {}
